4d8b70120641573d3c5c00e2d0cb6c271a017e61
Rui Marinho <me@ruimarinho.net>
2015-08-25 23:04:37 +0100

    Add SettingValueBindingHelper to help position the shapes in the canvas

--- BabySmash.Windows/MainPage.xaml.cs
+++ BabySmash.Windows/MainPage.xaml.cs
@@ -23,29 +23,13 @@ namespace BabySmash.Windows
 		public MainPage()
 		{
 			this.InitializeComponent();
-			var scope = BabySmash.Core.App.Container.BeginLifetimeScope();
-			// Resolve services from a scope that is a child
-			// of the root container.
-			DataContext = scope.Resolve<MainViewModel>();
 
-			ViewModel.Shapes.CollectionChanged += Shapes_CollectionChanged;
-		}
-
-		private void Shapes_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-		{
-			if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add) {
-
-				var shape = e.NewItems[0] as BabyShape;
-				var text = new TextBlock { Text = shape.ToString() };
-				text.FontSize = 22;
-				//text.Foreground = new SolidColorBrush(global::Windows.UI.Color.FromArgb((byte)shape.FillColor.A, (byte)shape.FillColor.R, (byte)shape.FillColor.G, (byte)shape.FillColor.B));
-				text.Foreground = new SolidColorBrush(global::Windows.UI.Colors.Red);
-
-				Canvas.SetTop(text, shape.Position.Y);
-				Canvas.SetLeft(text, shape.Position.X);
-
-				figuresCanvas.Children.Add(text);
+			using (var scope = BabySmash.Core.App.Container.BeginLifetimeScope()) {
+				// Resolve services from a scope that is a child
+				// of the root container.
+				DataContext = scope.Resolve<MainViewModel>();
 			}
+
 		}
 	}
 }
